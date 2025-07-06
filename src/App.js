a1dfaa9b0a7fe5737dc17275cc97fd98bce16bed
architapaul223 <archita.paul2125@gmail.com>
2025-07-06 17:00:40 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 // import logo from './logo.svg';
 // import './App.css';
-// import logo from './logo.svg';n
-// import './App.css';
+
 import About from './components/About';
-// import Footer1 from './Components/Footer1';
-// import Footer2 from './Components/Footer2';
+
 import Hero from './components/Hero';
 import NavBar from './components/NavBar';
-// import Projects from './Components/Projects';
-// import Skills from './Components/Skills';
+
 
 function App() {
   return (
@@ -19,8 +16,8 @@ function App() {
     <About/>
     {/* <Skills/>
     <Projects/>
-    <Footer1/>
-    <Footer2/> */}
+    <Footer/>
+     */}
 
    
     </>
