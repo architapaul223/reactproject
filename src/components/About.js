ec8c7d6607ed86c709d1668c9896abbfb382e216
architapaul223 <archita.paul2125@gmail.com>
2025-07-07 10:28:32 +0530

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -14,16 +14,45 @@ export default function About() {
           Currently, I’m working on sharpening my skills in React, JavaScript, and full-stack development. Along the way, 
           I’ve also developed a love for UI/UX design and content creation.
      </p>
-    <div class="card" style="width: 18rem;">
-  <div class="card-body">
-    <h5 class="card-title">Special title treatment</h5>
-    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
-
+  <div className="cardflex">
+     <div class="card" style={{width: '18rem'}}>
+     <div className="card-body">
+     <h5 className="card-title"> 1. UI/UX Design Exploration</h5>
+     <p className="card-text">I'm passionate about creating intuitive and visually appealing user experiences, constantly 
+      experimenting with layout, color, and interactivity.</p>
+     {/* <a href="#" className="btn btn-primary">Go somewhere</a> */}
+     {/* <button type="button" class="btn btn-primary">Primary</button> */}
      </div>
+     </div>
+      <div class="card" style={{width: '18rem'}}>
+     <div className="card-body">
+     <h5 className="card-title"> 2.Public Speaking & Communication</h5>
+     <p className="card-text">I have a passion for expressing ideas clearly and confidently. Whether it's presenting a project, leading a discussion, or participating in a group event, I enjoy connecting with people
+       through impactful communication. Public speaking has helped me grow as a leader, a listener, and a collaborator.</p>
+     {/* <a href="#" className="btn btn-primary">Go somewhere</a> */}
+     {/* <button type="button" class="btn btn-primary">Primary</button> */}
+     </div>
+     </div>
+     <div class="card" style={{width: '18rem'}}>
+     <div className="card-body">
+     <h5 className="card-title"> 3.Constant Learning & Tech Exploration</h5>
+     <p className="card-text">Technology is always evolving, and I love being part of that growth. I regularly explore new tools, frameworks, and trends—whether it’s a new CSS technique,
+       a JavaScript library, or a design tool. Learning keeps me motivated and helps me bring fresh ideas into my projects.</p>
+     {/* <a href="#" className="btn btn-primary">Go somewhere</a> */}
+     {/* <button type="button" class="btn btn-primary">Primary</button> */}
+     </div>
+     </div>
+
+
+ 
+   
+  </div>
+
+ 
+
+      </div>
 
   )
 }
+   
    
