229560391d818c17ec4a46ea8eba2f9dc854712c
architapaul223 <archita.paul2125@gmail.com>
2025-07-08 22:43:36 +0530

    Update Skills.js

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -5,12 +5,14 @@ import  "./Skills.css";
 export default function Skills() {
   return (
     <>
+    <section id="skills">
     <div>
     <div className="skillbox">
         {/* <h1 className="skillheading">Skills</h1> */}
         <img src={img2} alt="skill" className="skillimage"/>
     </div>
     </div>
+    </section>
     </>
   )
 }
