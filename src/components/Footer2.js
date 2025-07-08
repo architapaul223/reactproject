9c9d470848a4a2e85259782c89bf37a8c7e5f9bc
architapaul223 <archita.paul2125@gmail.com>
2025-07-08 21:54:33 +0530

    Create Footer2.js

---
+++ src/components/Footer2.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import  "./Footer2.css";
+
+export default function Footer2() {
+  return (
+   <>
+   <div className="footer-2">
+    <h6 className="copyright">&copy; Archita Paul.All rights reserved.</h6>
+   </div>
+   </>
+  )
+}
